837dae91033aac565ec7e873d0bf5056ced9dc90
Ramkrishna Saha <rajsahaking456@gmail.com>
2024-04-12 11:12:02 +0530

    service page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,207 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
+import "./Services.css";
 const Services = () => {
   return (
-    <div>
+    <div >
       <Navbar />
+      <div className="service-container">
+        <section className="service-header">
+          <h2>Areas of Expertise</h2>
+        </section>
+        <section className="grid-container">
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+
+      
+
+        </section>
+        <section className="grid-container-2">
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid-item-header">
+            <div className="grid-item-header-bar">
+
+            </div>
+            <h2>APPLICATIONS DEVELOPMENT & INTEGRATION</h2>
+          </div>
+          <div className="grid-item-list">
+            <ul>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+              <li>ERP (Oracle, SAP, Sarbanes Oxley PeopleSoft)</li>
+            </ul>
+          </div>
+        </div>
+        </section>
+        <section className="grid-container-3">
+          <div className="grid-container-3-desc">
+            <h2>We provide candidates on a Nationwide scale, now billing in 42 of the 50 states. We invest in taking the time to meet and maintain relationships across country as well as invest the time in learning the nuances for local markets.</h2>
+          </div>
+          <div className="grid-container-3-btn">
+            <span>FIND TALENT</span>
+            <span>FIND A CAREER</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
